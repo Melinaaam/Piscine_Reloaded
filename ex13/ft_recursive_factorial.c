78654817364bf29010a89acf1b75a66fb2618bbb
Melinaaam <mmo.melina@gmail.com>
2024-05-18 10:43:12 +0200

    change 1805

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -3,22 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: memotyle <memotyle@student.42.fr>          +#+  +:+       +#+        */
+/*   By: melinamotylewski <melinamotylewski@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 16:12:11 by memotyle          #+#    #+#             */
-/*   Updated: 2024/05/17 16:14:16 by memotyle         ###   ########.fr       */
+/*   Updated: 2024/05/18 10:41:49 by melinamotyl      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb == 0)
 		return (1);
 	if (nb > 0)
-		nb = nb * ft_recursive_factorial (nb - 1);
+		nb = nb * ft_recursive_factorial(nb - 1);
 	return (nb);
 }
-/* au 3eme if : rappel de la fonction qui va donner
-la valeur de n-1 dans le fonction (ici 5-1 = 4) etc…*/
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_recursive_factorial(13));
+	return (0);
+}
+au 3eme if : rappel de la fonction qui va donner
+la valeur de n-1 dans le fonction (ici 5-1 = 4) etc…
+*/
